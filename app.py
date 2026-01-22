d37897fd057f6fe4551c45c606732af8bd9889c5
Nazz <naziya8142@gmail.com>
2026-01-22 21:09:17 +0530

    app.py

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
+from student import Student
 
+def main():
+    students = []
 
+    while True:
+        print("\n1. Add Student")
+        print("2. View Students")
+        print("3. Exit")
 
-class Student:
-    def __init__(self, name, roll_no, marks):
-        self.name = name
-        self.roll_no = roll_no
-        self.marks = marks
+        choice = input("Enter your choice: ")
 
-    def display(self):
-        return f"Name: {self.name}, Roll No: {self.roll_no}, Marks: {self.marks}"
+        if choice == "1":
+            name = input("Enter name: ")
+            roll = input("Enter roll no: ")
+            marks = input("Enter marks: ")
+
+            student = Student(name, roll, marks)
+            students.append(student)
+            print("✅ Student added successfully")
+
+        elif choice == "2":
+            print("\n--- Student List ---")
+            for s in students:
+                print(s.display())
+
+        elif choice == "3":
+            print("Exiting program...")
+            break
+        else:
+            print("❌ Invalid choice")
+
+if __name__ == "__main__":
+    main()
 
